d4ccdcdb9db7550718bfc25ba0ddc67368ed0149
JAI-KAI <good003047@gmail.com>
2026-01-04 22:05:15 +0800

    test

--- components/test.vue
+++ components/test.vue
@@ -11,19 +11,23 @@ const props = defineProps(['id'])
 const obj = reactive({ count: 0 })
 const emit = defineEmits(['myEmit'])
 const theme = inject('theme')
-console.log(theme);
+// console.log(theme);
+
+//------------------watch------------------------
 watch(obj, (newValue, oldValue) => {
     // 在嵌套的屬性變更時觸發
     // 注意：`newValue` 此處和 `oldValue` 是相等的
     // 因為它們是同一個對象！
-    console.log(newValue, oldValue);
+    // console.log(newValue, oldValue);
 })
 obj.count++
 
+//監聽屬性值需要提供一個 getter 函數
 watch(() => props.id, (newValue, oldValue) => {
-    console.log(newValue);
+    // console.log(newValue);
 },
+    //強制偵聽器的回調立即執行
     { immediate: true }
 )
-console.log(testCount.count);
+// console.log(testCount.count);
 </script>
